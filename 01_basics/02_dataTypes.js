f1542402d51e5585fb976ba2cee09e9676dd0aaa
Mayank9569-hub <kakashimayank2021@gmail.com>
2025-05-26 19:46:37 +0000

    conversion of data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3)// We are using Nodejs, not browser
+
+console.log(3
+    +
+    3)// code readability should be high
+
+console.log("Mayank")
+
+
+let name = "Mayank"
+let age = 18
+let isloggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
